497d4837deebbd4d880a207a66e602c7d1c95701
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2018-10-06 20:10:09 +0530

    Update pzplot.m

--- pzplot.m
+++ pzplot.m
@@ -2,3 +2,5 @@ clear;
 clc;
 
 H = tf([1 2 1 1 4],[1 -1.5 0.5 3 0]);		%Transfer Function as mentioned in the question
+pzmap(H)									%This denotes the Pole Zero Plot of the above mentioned Transfer function
+grid on
